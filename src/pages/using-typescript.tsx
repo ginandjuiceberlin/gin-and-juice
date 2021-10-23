1cfb6702983daf5aed372fd9f43bde34316172bf
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2021-10-23 21:17:37 +0200

    refactor(root): :recycle: tidied up

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -1,17 +1,11 @@
 // If you don't want to use TypeScript you can delete this file!
 import * as React from "react";
-import { PageProps, Link, graphql } from "gatsby";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-type DataProps = {
-  site: {
-    buildTime: string;
-  };
-};
-
-const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
+const UsingTypescript = ({ data, path }) => (
   <Layout>
     <Seo title="Using TypeScript" />
     <h1>Gatsby supports TypeScript by default!</h1>
