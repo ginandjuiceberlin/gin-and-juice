009e07bcec403d4888550e628a4fe7404f213cd6
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2021-10-25 21:26:28 +0200

    feat(root): :sparkles: add google fonts plugin and varela round font, use it on at least one place

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,15 @@ module.exports = {
         failOnError: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Varela Round`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
